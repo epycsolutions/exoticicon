30d219bc81f7a9e21a31194f92240bc0e01b4676
David Stüwe <wurmlitv@gmail.com>
2023-12-07 13:19:20 +0100

    Created index and constants file

---
+++ src/constants.ts
@@ -0,0 +1,40 @@
+import { resolve } from 'path'
+
+import { RunnerOptions } from './types/runner'
+import { FontAssetType, OtherAssetType } from './types/misc'
+
+import { getIconId } from './utils/icon-id'
+
+export const TEMPLATES_DIR = resolve(__dirname, '../templates')
+
+export const DEFAULT_OPTIONS: Omit<RunnerOptions, 'inputDir' | 'outputDir'> = {
+    name: 'icons',
+    fontTypes: [
+        FontAssetType.EOT,
+        FontAssetType.WOFF2,
+        FontAssetType.WOFF
+    ],
+    assetTypes: [
+        OtherAssetType.CSS,
+        OtherAssetType.HTML,
+        OtherAssetType.JSON,
+        OtherAssetType.TS
+    ],
+    formatOptions: {
+        json: { indent: 4 }
+    },
+    pathOptions: {},
+    templates: {},
+    codepoints: {},
+    round: undefined,
+    fontHeight: 300,
+    descent: undefined,
+    normalize: undefined,
+    selector: null,
+    tag: 'i',
+    prefix: 'icon',
+    fontsUrl: undefined,
+    getIconId: getIconId
+}
+
+export const DEFAULT_START_CODEPOINT = 0xf101
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export {
+    FontAssetType,
+    OtherAssetType,
+    ASSET_TYPES,
+    AssetType,
+    GetIconIdFn,
+    GetIconIdOptions
+} from './types/misc'
+
+export { RunnerOptions } from './types/runner'
+
+export { generateFonts, generateFonts as default } from './core/runner'
