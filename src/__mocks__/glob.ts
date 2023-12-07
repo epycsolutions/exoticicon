828caac0e6a6014c249d4bd957b5cabfcfacae8c
David Stüwe <wurmlitv@gmail.com>
2023-12-07 02:42:01 +0100

    Created ``mocks``

---
+++ src/__mocks__/glob.ts
@@ -0,0 +1,25 @@
+const mockGlobs = {
+    './valid/**/*.svg': [
+        '/project/valid/foo.svg',
+        '/project/valid/bar.svg',
+        '/project/valid/sub/nested.svg',
+        '/project/valid/sub/sub/nested.svg'
+    ],
+    './empty/**/*.svg': []
+}
+
+module.exports = (
+    glob: string,
+    _: {},
+    callback: (err: Error | null, paths: string[] | null) => void
+) => {
+    setTimeout(() => {
+        const paths = mockGlobs[glob]
+
+        if(!paths) {
+            return callback(new Error(`Invalid glob: ${ glob }`), null)
+        }
+        
+        return callback(null, paths)
+    })
+}
