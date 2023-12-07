91ca8b9b0576c584a48cbee82aa64eef98c786df
David Stüwe <wurmlitv@gmail.com>
2023-12-07 02:42:44 +0100

    Added some more utils

---
+++ src/utils/assets.ts
@@ -0,0 +1,97 @@
+import glob from 'glob'
+import { promisify } from 'util'
+import { resolve, relative, join } from 'path'
+
+import { removeExtension, splitSegments } from './path'
+import { writeFile } from './fs-async'
+
+import { RunnerOptions } from '../types/runner'
+import { GeneratedAssets } from '../generators/generate-assets'
+
+export type WriteResult = { content: string | Buffer; writePath: string }
+export type WriteResults = WriteResult[]
+
+export interface IconAsset {
+    id: string
+    absolutePath: string
+    relativePath: string
+}
+
+export interface AssetsMap {
+    [key: string]: IconAsset
+}
+
+export const ASSET_EXTENSION = 'svg'
+
+export const loadPaths = async (dir: string): Promise<string[]> => {
+    const globPath = join(dir, `**/*.${ ASSET_EXTENSION }`)
+    const files = await promisify(glob)(globPath, {})
+
+    if(!files.length) {
+        throw new Error(`No SVGs found in ${ dir }`)
+    }
+
+    return files
+}
+
+const failForConflictingId = (
+    { relativePath: pathA, id }: IconAsset,
+    { relativePath: pathB }: IconAsset
+): void => {
+    throw new Error(
+        `Conflicting result from 'getIconId': '${ id }' - conflicting input files:\n` +
+        [pathA, pathB].map((fPath) => `  - ${ fPath }`).join('\n')
+    )
+}
+
+export const loadAssets = async ({
+    inputDir,
+    getIconId
+}: RunnerOptions): Promise<AssetsMap> => {
+    const paths = await loadPaths(inputDir)
+    const out = {}
+    let index = 0
+
+    for(const path of paths) {
+        const relativePath = relative(resolve(inputDir), resolve(path))
+        const parts = splitSegments(relativePath)
+        const basename = removeExtension(parts.pop())
+        const absolutePath = resolve(path)
+        const iconId = getIconId({
+            basename,
+            relativeDirPath: join(...parts),
+            absoluteFilePath: absolutePath,
+            relativeFilePath: relativePath,
+            index
+        })
+
+        const result: IconAsset = { id: iconId, relativePath, absolutePath }
+
+        if(out[iconId]) {
+            failForConflictingId(out[iconId], result)
+        }
+
+        out[iconId] = result
+
+        index++
+    }
+
+    return out
+}
+
+export const writeAssets = async (
+    assets: GeneratedAssets,
+    { name, pathOptions = {}, outputDir }: RunnerOptions
+) => {
+    const results: WriteResults = []
+
+    for(const ext of Object.keys(assets)) {
+        const filename = [name, ext].join('.')
+        const writePath = pathOptions[ext] || join(outputDir, filename)
+        
+        results.push({ content: assets[ext], writePath })
+        await writeFile(writePath, assets[ext])
+    }
+
+    return results
+}
---
+++ src/utils/codepoints.ts
@@ -0,0 +1,36 @@
+import { DEFAULT_START_CODEPOINT } from '../constants'
+import { AssetsMap } from './assets'
+
+export type CodepointsMap = { [key: string]: number }
+
+export const getCodepoints = (
+    assets: AssetsMap,
+    predefined: CodepointsMap = {},
+    start = DEFAULT_START_CODEPOINT
+): CodepointsMap => {
+    const out: CodepointsMap = {}
+    const used = Object.values(predefined)
+    let current: number = start
+
+    const getNextCodepoint = () => {
+        while(used.includes(current)) {
+            current++
+        }
+
+        const res = current
+        current++
+        
+        return res
+    }
+
+    for(const id of Object.keys(assets)) {
+        if(!predefined[id]) {
+            out[id] = getNextCodepoint()
+        }
+    }
+
+    return { ...predefined, ...out }
+}
+
+export const getNextCodepoint = (decimalCodepoint: number): string =>
+    decimalCodepoint.toString(16)
