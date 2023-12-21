a8d7c7a91871d8710d3953aa155208159eb9bed1
David Stüwe <wurmlitv@gmail.com>
2023-12-21 19:11:47 +0100

    Created tests for core

---
+++ src/core/__tests__/config-parser.ts
@@ -0,0 +1,115 @@
+import { parseConfig } from '../config-parser'
+import { checkPath } from '../../utils/fs-async'
+import { DEFAULT_OPTIONS } from '../../constants'
+
+const checkPathMock = checkPath as any as jest.Mock
+
+jest.mock('../../utils/fs-async', () => ({ checkPath: jest.fn() }))
+
+jest.mock('../../types/misc', () => ({
+    FontAssetType: { svg: 'a', eot: 'b' },
+    OtherAssetType: { svg: 'c', eot: 'd' }
+}))
+
+const mockConfig = {
+    inputDir: '/root',
+    outputDir: '/root',
+    name: 'foo',
+    fontTypes: ['a', 'b'],
+    assetTypes: ['c', 'd'],
+    formatOptions: { svg: { foo: 'bar' } },
+    pathOptions: { css: '/dev/null' },
+    codepoints: { foo: 'bar' },
+    fontHeight: 1,
+    descent: 2,
+    normalize: true,
+    round: 3,
+    selector: null,
+    tag: 'f',
+    prefix: 'baz',
+    fontsUrl: '/fonts',
+    templates: {
+        css: 'css',
+        sass: 'sass',
+        scss: 'scss',
+        html: 'html'
+    },
+    getIconId: DEFAULT_OPTIONS.getIconId
+}
+
+const testError = async (options: object, key: string, message: string) =>
+    expect(() => parseConfig({ ...mockConfig, ...options })).rejects.toThrow(
+        `Invalid option ${key}: ${message}`
+    )
+
+const testParsed = async (key: string, input: any, output: any) =>
+    expect((await parseConfig({ ...mockConfig, [key]: input }))[key]).toEqual(
+        output
+    )
+
+describe('Config parser', () => {
+    beforeEach(() => {
+        checkPathMock.mockClear()
+        checkPathMock.mockImplementation(() => Promise.resolve(true))
+    })
+
+    test('returns correctly parsed input when valid', async () => {
+        expect(await parseConfig({ ...mockConfig })).toEqual({ ...mockConfig })
+    })
+
+    test('corretly parses acceptable values', async () => {
+        await testParsed('descent', undefined, undefined)
+        await testParsed('descent', '1', 1)
+
+        await testParsed('normalize', 'true', true)
+        await testParsed('normalize', '1', true)
+        await testParsed('normalize', 1, true)
+    })
+
+    test('throws expected validation errors when given invalid input', async () => {
+        await testError({ inputDir: 2 }, 'inputDir', '2 is not a string')
+        await testError(
+            { inputDir: {} },
+            'inputDir',
+            '[object Object] is not a string'
+        )
+
+        await testError(
+            { fontTypes: ['x'] },
+            'fontTypes',
+            'x is not valid - accepted values are: a, b'
+        )
+        await testError(
+            { assetTypes: ['x'] },
+            'assetTypes',
+            'x is not valid - accepted values are: c, d'
+        )
+
+        await testError({ descent: 'a' }, 'descent', 'a is not a valid number')
+        await testError(
+            { normalize: null },
+            'normalize',
+            'must be a boolean value'
+        )
+
+        await testError({ getIconId: true }, 'getIconId', 'true is not a function')
+    })
+
+    test('correctly validates existance of input and output paths', async () => {
+        checkPathMock.mockImplementationOnce(() => Promise.resolve(false))
+        await testError({ inputDir: 'foo' }, 'inputDir', 'foo is not a directory')
+
+        checkPathMock.mockImplementation((val) => Promise.resolve(val !== 'bar'))
+        await testError(
+            { outputDir: 'bar' },
+            'outputDir',
+            'bar is not a directory'
+        )
+    })
+
+    test('throws expected error when passing an unrecognised option', async () => {
+        await expect(() => parseConfig({ ...mockConfig, foo: 'bar' })).rejects.toThrow(
+            "The option 'foo' is not recognised"
+        )
+    })
+})
---
+++ src/core/__tests__/runner.ts
@@ -0,0 +1,174 @@
+import { generateFonts } from '../runner'
+import { parseConfig } from '../config-parser'
+
+import { writeAssets, loadAssets } from '../../utils/assets'
+import { generateAssets } from '../../generators'
+import { getGeneratorOptions } from '../../generators/generator-options'
+
+import { DEFAULT_OPTIONS } from '../../constants'
+
+const generateAssetsMock = generateAssets as any as jest.Mock;
+const parseConfigMock = parseConfig as any as jest.Mock;
+const writeAssetsMock = writeAssets as any as jest.Mock;
+const loadAssetsMock = loadAssets as any as jest.Mock;
+const getGeneratorOptionsMock = getGeneratorOptions as any as jest.Mock;
+
+jest.mock('../../constants', () => ({
+    DEFAULT_OPTIONS: { hasDefaults: true, parsed: false }
+}))
+
+jest.mock('../../generators', () => ({
+    generateAssets: jest.fn((options) =>
+        Promise.resolve({ mockGenerated: { assets: {}, options } })
+    )
+}))
+
+jest.mock('../../generators/generator-options', () => ({
+    getGeneratorOptions: jest.fn(() => ({ mock: 'generator-options' }))
+}))
+
+jest.mock('../../utils/assets', () => ({
+    writeAssets: jest.fn(() => Promise.resolve([{ mock: 'writeResult' }])),
+    loadAssets: jest.fn(() => Promise.resolve({ mock: 'assets' }))
+}))
+
+jest.mock('../config-parser', () => ({
+    parseConfig: jest.fn((config = {}) => ({ ...config, parsed: true }))
+}))
+
+describe('Runner', () => {
+    beforeEach(() => {
+        writeAssetsMock.mockClear()
+        loadAssetsMock.mockClear()
+        generateAssetsMock.mockClear()
+        parseConfigMock.mockClear()
+        getGeneratorOptionsMock.mockClear()
+    })
+
+    test('`generateFonts` resolves with expected results', async () => {
+        const optionsIn = { inputDir: 'foo' } as any
+        const optionsOut = { hasDefaults: true, parsed: true, inputDir: 'foo' }
+
+        expect(await generateFonts(optionsIn)).toEqual({
+            options: optionsOut,
+            writeResults: [],
+            assetsIn: { mock: 'assets' },
+            assetsOut: {
+                mockGenerated: {
+                    assets: {},
+                    options: { mock: 'generator-options' }
+                }
+            }
+        })
+    })
+
+    test('`generateFonts` resolves with asset write result if `outputDir` was passed', async () => {
+        const optionsIn = { inputDir: 'foo', outputDir: 'foo' } as any
+        const optionsOut = {
+            hasDefaults: true,
+            parsed: true,
+            inputDir: 'foo',
+            outputDir: 'foo'
+        }
+
+        expect(await generateFonts(optionsIn)).toEqual({
+            options: optionsOut,
+            writeResults: [{ mock: 'writeResult' }],
+            assetsIn: { mock: 'assets' },
+            assetsOut: {
+                mockGenerated: { assets: {}, options: { mock: 'generator-options' } }
+            }
+        })
+    })
+
+    test('`generateFonts` throws error if `outputDir` is not given and `mustWrite` is `true`', async () => {
+        const optionsIn = { inputDir: 'foo' } as any
+
+        await expect(() => generateFonts(optionsIn, true)).rejects.toThrow(
+            'You must specify an output directory'
+        )
+    })
+
+    test('`generateFonts` throws error if `inputDir` is not given', async () => {
+        const optionsIn = {} as any
+
+
+        await expect(() => generateFonts(optionsIn)).rejects.toThrow(
+            'You must specify an input directory'
+        )
+    })
+
+    test('`generateFonts` calls `parseConfig` correctly', async () => {
+        const optionsIn = { inputDir: 'foo', foo: 'bar' } as any
+
+        await generateFonts(optionsIn)
+
+        expect(parseConfigMock).toHaveBeenCalledTimes(1)
+        expect(parseConfigMock).toHaveBeenCalledWith({
+            ...DEFAULT_OPTIONS,
+            ...optionsIn
+        })
+    })
+
+    test('`generateFonts` calls `loadAssets` correctly', async () => {
+        const optionsIn = { inputDir: '/dev/in' } as any
+
+        await generateFonts(optionsIn)
+        
+        expect(loadAssetsMock).toHaveBeenCalledTimes(1)
+        expect(loadAssetsMock).toHaveBeenCalledWith({
+            ...DEFAULT_OPTIONS,
+            ...optionsIn,
+            parsed: true
+        })
+    })
+
+    test('`generateFonts` calls `getGeneratorOptions` correctly', async () => {
+        const optionsIn = { inputDir: '/dev/in' } as any;
+
+        await generateFonts(optionsIn)
+
+        expect(getGeneratorOptionsMock).toHaveBeenCalledTimes(1)
+        expect(getGeneratorOptionsMock).toHaveBeenCalledWith(
+            {
+                hasDefaults: true,
+                inputDir: '/dev/in',
+                parsed: true
+            },
+            {
+                mock: 'assets'
+            }
+        )
+    })
+
+    test('`generateFonts` calls `generateAssets` correctly', async () => {
+        const optionsIn = {  inputDir: 'foo', bar: 'bar' } as any
+
+        await generateFonts(optionsIn)
+
+        expect(generateAssetsMock).toHaveBeenCalledTimes(1)
+        expect(generateAssetsMock).toHaveBeenCalledWith({
+            mock: 'generator-options'
+        })
+    })
+
+    test('`generateFonts` calls `writeAssets` correctly', async () => {
+        const optionsIn = { outputDir: 'foo/bar', inputDir: 'foo' } as any
+
+        await generateFonts(optionsIn)
+
+        expect(writeAssetsMock).toHaveBeenCalledTimes(1)
+        expect(writeAssetsMock).toHaveBeenCalledWith(
+            { mockGenerated: { assets: {}, options: { mock: 'generator-options' } } },
+            { hasDefaults: true, inputDir: 'foo', outputDir: 'foo/bar', parsed: true }
+        )
+    })
+
+    test('`generateFonts` does not call `writeAssets` if `outputDir` is not specified', async () => {
+        const optionsIn = { inputDir: 'foo' } as any
+
+        await generateFonts(optionsIn)
+
+        expect(writeAssetsMock).not.toHaveBeenCalled();
+    })
+})
